ff303410c01e672c6b2b4658db5cf8e0a04889e6
Milos Marinkovic <marinkovic.milos9816@gmail.com>
2019-11-13 00:10:33 +0100

    Initial commit | Added .gitignore file and removed node_modules

---
+++ src/events.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+function createRouter(db) {
+    const router = express.Router();
+    const owner = '';
+
+    // Routes are defined here
+
+    return router;
+}
+
+module.exports = createRouter;
